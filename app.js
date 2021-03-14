55c64fde69fa177ad71ffc34db082195fa8e68df
Tanmay Arora <67386710+tanmayarora11@users.noreply.github.com>
2021-03-15 00:12:52 +0530

    refractored

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 const express=require("express");
-// const path=require('path');
-// const bodyParser=require('body-parser');
-// const ejs=require('ejs');
 const mongoose=require("mongoose");
 
 const app = express();
